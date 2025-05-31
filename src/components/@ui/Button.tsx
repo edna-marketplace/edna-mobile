8698cc88502ad8465d749d921cd2285c0359b687
matheusbarcc <matheusbarcon@gmail.com>
2025-05-31 11:56:45 -0300

    style: add icons to the button component

--- src/components/@ui/Button.tsx
+++ src/components/@ui/Button.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 
 import {
   ButtonSpinner,
@@ -6,22 +6,31 @@ import {
   Text,
 } from "@gluestack-ui/themed";
 
+import { Icon } from "phosphor-react-native";
+import { gluestackUIConfig } from "../../../config/gluestack-ui.config";
+
 type Props = ComponentProps<typeof GluestackButton> & {
   title: string;
   variantStyle?: "primary" | "secondary" | "tertiary";
   isLoading?: boolean;
+  children?: ReactNode;
+  icon?: Icon;
 };
 
 export function Button({
   title,
   variantStyle = "primary",
   isLoading = false,
+  icon: IconComponent,
   ...rest
 }: Props) {
+  const theme = gluestackUIConfig.tokens.colors;
+
   return (
     <GluestackButton
       w="$full"
       h="$14"
+      gap="$1"
       bg={
         variantStyle === "primary"
           ? "$base100"
@@ -41,16 +50,26 @@ export function Button({
       {...rest}
     >
       {isLoading ? (
-        <ButtonSpinner />
-      ) : (
-        <Text
+        <ButtonSpinner
           color={variantStyle === "primary" ? "$white" : "$base100"}
-          fontFamily="$heading"
-          fontSize="$sm"
-          fontWeight={variantStyle === "tertiary" ? "$bold" : "$regular"}
-        >
-          {title}
-        </Text>
+        />
+      ) : (
+        <>
+          {IconComponent && (
+            <IconComponent
+              color={variantStyle === "primary" ? theme.white : theme.base100}
+              weight={variantStyle === "tertiary" ? "bold" : "regular"}
+            />
+          )}
+          <Text
+            color={variantStyle === "primary" ? "$white" : "$base100"}
+            fontFamily="$heading"
+            fontSize="$sm"
+            fontWeight={variantStyle === "tertiary" ? "$bold" : "$regular"}
+          >
+            {title}
+          </Text>
+        </>
       )}
     </GluestackButton>
   );
--- src/screens/ClotheCategories.tsx
+++ src/screens/ClotheCategories.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/@ui/Button";
 import { Center } from "@gluestack-ui/themed";
+import BookmarkSimple from "phosphor-react-native/src/icons/BookmarkSimple";
 
 export function ClotheCategories() {
   return (
     <Center flex={1} bg="$base700" px="$6" gap="$6">
-      <Button title="Click" />
-      <Button title="Click" variantStyle="secondary" />
-      <Button title="Click" variantStyle="tertiary" />
+      <Button title="Click" icon={BookmarkSimple} />
+      <Button title="Click" variantStyle="secondary" icon={BookmarkSimple} />
+      <Button title="Click" variantStyle="tertiary" icon={BookmarkSimple} />
     </Center>
   );
 }
