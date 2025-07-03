298a26038193e123fa134db32084c2ddb0f620a2
matheusbarcc <matheusbarcon@gmail.com>
2025-07-03 13:47:44 -0300

    feat: add password update

--- src/components/UpdatePassword.tsx
+++ src/components/UpdatePassword.tsx
@@ -8,31 +8,23 @@ import ArrowsClockwise from "phosphor-react-native/src/icons/ArrowsClockwise";
 import { Button } from "./@ui/Button";
 import { AppError } from "@/utils/AppError";
 import { ToastMessage } from "./ToastMessage";
+import { PasswordInput } from "./@ui/PasswordInput";
 
-export const UpdatePasswordSchema = yup
-  .object({
-    oldPassword: yup
-      .string()
-      .min(6, "A senha tem no mínimo 6 caracteres")
-      .required(),
-    newPassword: yup
-      .string()
-      .min(6, "A senha deve ter no mínimo 6 caracteres")
-      .required(),
-  })
-  .test(
-    "passwords-different",
-    "A nova senha deve ser diferente da antiga",
-    function (value) {
-      return value.oldPassword !== value.newPassword;
-    }
-  );
+export const UpdatePasswordSchema = yup.object({
+  oldPassword: yup
+    .string()
+    .min(6, "A senha tem no mínimo 6 caracteres")
+    .required(),
+  newPassword: yup
+    .string()
+    .min(6, "A senha deve ter no mínimo 6 caracteres")
+    .required(),
+});
 
 export type UpdatePasswordData = yup.InferType<typeof UpdatePasswordSchema>;
 
 export function UpdatePassword() {
   const {
-    register,
     handleSubmit,
     control,
     reset,
@@ -81,26 +73,30 @@ export function UpdatePassword() {
         name="oldPassword"
         control={control}
         render={({ field: { onChange, value } }) => (
-          <Input
+          <PasswordInput
             onChangeText={onChange}
+            maxLength={15}
             value={value}
             label="Senha antiga"
             placeholder="Sua senha antiga"
+            secureTextEntry
             errorMessage={errors.oldPassword?.message}
           />
         )}
       />
 
       <Controller
-        name="oldPassword"
+        name="newPassword"
         control={control}
         render={({ field: { onChange, value } }) => (
-          <Input
+          <PasswordInput
             onChangeText={onChange}
+            maxLength={15}
             value={value}
-            label="Senha antiga"
-            placeholder="Sua senha antiga"
-            errorMessage={errors.oldPassword?.message}
+            label="Senha nova"
+            placeholder="Sua senha nova"
+            secureTextEntry
+            errorMessage={errors.newPassword?.message}
           />
         )}
       />
